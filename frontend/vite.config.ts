7070dcf50633d805b97844c2eeac486c5967698c
tomaslsaaavedrarhost <tomaslsaaavedra@rhost.cl>
2025-02-20 19:49:23 -0600

    chore: configuración de GitHub Pages y GitHub Actions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/form-onboarding/',
   server: {
     port: 3000,
     strictPort: true,
